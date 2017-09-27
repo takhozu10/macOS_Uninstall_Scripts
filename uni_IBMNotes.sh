adfb59cfebd9626ea2c3ab5933babc4e552c3c35
takhozu10 <thozumi@biglots.com>
2017-09-27 17:13:08 -0400

    Initial Commit

--- uni_IBMNotes.sh
+++ uni_IBMNotes.sh
@@ -5,18 +5,37 @@
 #Big Lots, Inc.
 #Uninstall IBM Notes.
 
-notes=$(ps aux | grep 'Visual Studio Code' -c)
+notes=$(ps aux | grep 'IBM Notes' -c)
+if [ test -d /Applications/Lotus Notes.app ]
+    then
+        lotusnotes=$(ps aux | grep 'Lotus Notes' -c)
+        if [ $lotusnotes -gt 1 ]
+            then
+            echo "Lotus Notes version 8 is installed on this computer"
+            #Identified that the older verison of Lotus Notes is installed on this computer.
+            lotusnotesps=$(ps aux | grep 'Lotus Notes' | grep -v grep | awk '{print $2}')
+            kill $lotusnotes
+            echo "Terminating Lotus Notes app"
+            sleep 3
+            rm -rfv /Applications/Lotus\ Notes.app
+            rm -rfv /Applications/Lotus\ Application\ Support
+            rm -rfv ~/Library/Application\ Support/Lotus\ Notes\ Data
+            rm -rfv ~/Library/Preferences/Notes\ Preferences
+            rm -rfv /Library/Receipts/Lotus\ Notes\ Installer_English.pkg
+            rm -rfv /Library/Receipts/Lotus\ Notes\ Installer.pkg
+            rm -rfv /Library/Receipts/xpdcoreinstaller.pkg
+
 if [ $notes -gt 1 ]
     then
         echo "IBM Notes app is currently running on the computer"
         #Terminate IBM Notes app and uninstall IBM Notes and its configuration files.
-        visualps=$(ps aux | grep 'Visual Studio Code.app/Contents/MacOS/Electron' | grep -v grep | awk '{print $2}')
-        kill $visualps
-        echo "Terminating Visual Studio Code app"
+        ibmnotes=$(ps aux | grep 'IBM Notes' | grep -v grep | awk '{print $2}')
+        kill $ibmnotes
+        echo "Terminating IBM notes app"
         sleep 3
-        rm -rf /Applications/IBM\ Notes.app
-        rm -rf ~/Library/Application\ Support/IBM\ Notes\ Data
-        rm -rf ~/Library/Preferences/Notes\ Preferences
+        rm -rfv /Applications/IBM\ Notes.app
+        rm -rfv ~/Library/Application\ Support/IBM\ Notes\ Data
+        rm -rfv ~/Library/Preferences/Notes\ Preferences
         echo "Uninstall Completed"
     else
         echo "The Visual Studio Code app is not running on the computer"
