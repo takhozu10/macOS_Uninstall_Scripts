e6bc61bb0b8bf4de8fb28e56e7f9e2460802a3b3
takhozu10 <thozumi@biglots.com>
2018-03-20 13:44:30 -0400

    Updated Licensing

--- uni_IBMNotes.sh
+++ uni_IBMNotes.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
+# 
+# # #
+# MIT License
 #
+# Copyrigth (c) 2018 Big Lots Stores, Inc. 
 #
-#Created by Tak Hozumi.
-#Big Lots, Inc.
-#Uninstall IBM Notes.
+#   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+#   associated documentation files (the "Software"), to deal in the Software without restriction, including 
+#   without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+#   following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in all copies or substantial
+#   portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+#   NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+#   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+#   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# # #
+
+# # #
+# This script removes Lotus Notes version 8 or IBM Notes version 9 from a computer. 
+# It will remove all preferences and IBM Notes Data along with the Notes application.
+#
+# Written by: Tak Hozumi : End User Support Engineer : Big Lots Stores, Inc.
+#
+# Created on: October 17, 2017
+# Modified on: October 17, 2017
+# Version: 0.1.0
+# # #
 
 if [ -d /Applications/Lotus\ Notes.app ]
 then
