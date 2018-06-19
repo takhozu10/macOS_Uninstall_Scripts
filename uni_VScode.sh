bb1bc9d7775877f235edf91a35055287017c4cb3
takhozu10 <thozumi@biglots.com>
2018-06-19 16:09:31 -0400

    Added ~/Library/Application Support/Code directory from removal

--- uni_VScode.sh
+++ uni_VScode.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
+# 
+# # #
+# MIT License
+#
+# Copyrigth (c) 2018 Big Lots Stores, Inc. 
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+#   associated documentation files (the "Software"), to deal in the Software without restriction, including 
+#   without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+#   following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in all copies or substantial
+#   portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+#   NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+#   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+#   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# # #
+
+# # #
+# Uninstall Visual Studio Code
 #
 #
-#Created by Tak Hozumi.
-#Big Lots, Inc.
-#Uninstall VSCode.
+# Written by: Tak Hozumi : End User Support Engineer : Big Lots Stores, Inc.
+#
+# Created on: 05/18/2017
+# Modified on: 06/19/2018
+# Version: 0.0.1
+# # #
 
 VSCode=$(ps aux | grep 'Visual Studio Code' -c)
 if [ $VSCode -gt 1 ]
@@ -15,6 +42,7 @@ if [ $VSCode -gt 1 ]
         echo "Terminating Visual Studio Code app"
         sleep 3
         rm -rf /Applications/Visual\ Studio\ Code.app
+        rm -rf ~/Library/Application\ Support/Code
         rm -rf ~/Library/Caches/com.microsoft.VSCode
         rm -rf ~/Library/Caches/com.microsoft.VSCode.ShipIt
         rm -rf ~/Library/Preferences/com.microsoft.VSCode.helper.plist
