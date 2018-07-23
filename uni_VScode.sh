c2c490962d8a2cc0af1819d01ef4b1b264418ebc
takhozu10 <thozumi@biglots.com>
2018-07-23 11:17:12 -0400

    Reviewed the code for compatiblity with VSCode 1.25.1

--- uni_VScode.sh
+++ uni_VScode.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# 
+#
 # # #
 # MIT License
 #
-# Copyrigth (c) 2018 Big Lots Stores, Inc. 
+# Copyrigth (c) 2018 Big Lots Stores, Inc.
 #
-#   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
-#   associated documentation files (the "Software"), to deal in the Software without restriction, including 
+#   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+#   associated documentation files (the "Software"), to deal in the Software without restriction, including
 #   without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 #   copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
 #   following conditions:
@@ -15,9 +15,9 @@
 #   portions of the Software.
 #
 #   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-#   NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-#   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+#   NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 #   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # # #
 
@@ -37,6 +37,7 @@ if [ $VSCode -gt 1 ]
     then
         echo "The Visual Studio Code app is currently running on the computer"
         #Terminate Visual Studio Code app and uninstall Visual Studio Code and its configuration files.
+        #Assign VS Code process ID to visualps variable. Run kill command using the process ID to terminate the application.
         visualps=$(ps aux | grep 'Visual Studio Code.app/Contents/MacOS/Electron' | grep -v grep | awk '{print $2}')
         kill $visualps
         echo "Terminating Visual Studio Code app"
